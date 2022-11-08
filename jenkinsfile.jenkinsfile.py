24a6105f9e912e20126b1156777ae2d63f1534ba
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 11:27:25 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -6,7 +6,6 @@ pipeline {
         echo 'Installing required pyhon libraries'
         sh 'pip3 install robotframework'
         sh 'pip3 install robotframework-databaselibrary'
-        sh 'pip3 install pymssql --no-binary :all'
         sh 'pip3 install pyodbc'
         sh 'pip3 install pytest'
       }
