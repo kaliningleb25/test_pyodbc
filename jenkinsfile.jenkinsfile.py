c953fc1d91d1ffaa3cfaddd80942402ab64a33ef
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 13:44:44 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -17,8 +17,8 @@ pipeline {
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic 
 main" | tee /etc/apt/sources.list.d/mssql-release.list
-apt update
-apt install msodbcsql17
+apt-get update
+apt-get install msodbcsql17
 
 '''
             }
