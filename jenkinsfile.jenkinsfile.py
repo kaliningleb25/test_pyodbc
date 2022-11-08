1421cc46e7f74bbaf7ddedf27603bd251f54826c
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 13:36:28 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -13,7 +13,21 @@ pipeline {
     }
     stage('Test') {
         steps {
-            sh 'cat /etc/odbcinst.ini'
+            sh '''
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
+
+'''
             }
         }
     stage('PyTest') {
