873be7f87cfddae1a8bb806e0894250d5a1f1d78
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 13:08:59 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -11,6 +11,31 @@ pipeline {
         sh 'pip3 install pytest'
       }
     }
+    stage('Install') {
+        steps {
+            sh'''
+            if ! [[ "18.04 20.04 22.04" == *"$(lsb_release -rs)"* ]];
+then
+    echo "Ubuntu $(lsb_release -rs) is not currently supported.";
+    exit;
+fi
+
+sudo su
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
+
+curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
+
+exit
+sudo apt-get update
+sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
+source ~/.bashrc
+# optional: for unixODBC development headers
+sudo apt-get install -y unixodbc-dev
+'''
+        }
+
+      }  
     stage('PyTest') {
       steps {
         sh 'python3 -m pytest dq.py'
