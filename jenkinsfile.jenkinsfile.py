9716ab16bef678481667aed40618722ebb6aa8d3
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 13:46:22 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -11,18 +11,6 @@ pipeline {
         sh 'pip3 install pytest'
       }
     }
-    stage('Test') {
-        steps {
-            sh '''
-curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-echo "deb [arch=amd64] https://packages.microsoft.com/ubuntu/18.04/prod bionic 
-main" | tee /etc/apt/sources.list.d/mssql-release.list
-apt-get update
-apt-get install msodbcsql17
-
-'''
-            }
-        }
     stage('PyTest') {
       steps {
         sh 'python3 -m pytest dq.py'
