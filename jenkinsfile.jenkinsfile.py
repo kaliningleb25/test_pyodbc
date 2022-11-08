56804c25d9a92534b75b46017aa8f16fd6c0d2bb
Gleb Kalinin <mcwanted@mail.ru>
2022-11-08 13:39:10 -0600

    change

--- jenkinsfile.jenkinsfile.py
+++ jenkinsfile.jenkinsfile.py
@@ -14,18 +14,17 @@ pipeline {
     stage('Test') {
         steps {
             sh '''
-sudo su
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 
 curl https://packages.microsoft.com/config/ubuntu/$(lsb_release -rs)/prod.list > /etc/apt/sources.list.d/mssql-release.list
 
 exit
-sudo apt-get update
-sudo ACCEPT_EULA=Y apt-get install -y msodbcsql18
+apt-get update
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
 echo 'export PATH="$PATH:/opt/mssql-tools18/bin"' >> ~/.bashrc
 source ~/.bashrc
 # optional: for unixODBC development headers
-sudo apt-get install -y unixodbc-dev
+apt-get install -y unixodbc-dev
 
 '''
             }
